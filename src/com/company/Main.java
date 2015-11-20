2b0a28bd983c163ef6591e9fc2c0ba41efe81c24
tulipan <modrzejewskimichal@wp.pl>
2015-11-20 17:29:55 +0100

    string

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,21 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("TESCIK");
-        String s= "przykladowy string";
+        String s = "przykladowy string a d g";
         System.out.println(s);
-
-
-	// write your code here
+        if (s == null || s.isEmpty())
+            System.out.println("0");
+        int a = 0;
+        for (int b = 0; b < s.length(); b++) {
+            if (s.charAt(b) != ' ') {
+                a++;
+                while (s.charAt(b) != ' ' && b < s.length() - 1) {
+                    b++;
+                }
+            }
+        }
+        System.out.println(a);
+        // write your code here
     }
+
 }
